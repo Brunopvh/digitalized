ed0553ef3b709226ffaaf97a7ef61aff977ac023
Brunopvh <brunodasill@gmail.com>
2025-12-28 22:25:36 -0400

    Correção tentativa 3

--- app_variacao/documents/sheet/__init__.py
+++
@@ -1,5 +0,0 @@
-from .excel import ReadSheetExcel, ExcelLoad
-from .ods import ReadSheetODS, ODSLoad
-from .types import SheetData, WorkbookData, RowSheetIterator, SheetIndexNames
-from .xml import read_zip_xml, WorkbookMappingXML
-from .csv import ReadSheetCsv, CsvLoad
